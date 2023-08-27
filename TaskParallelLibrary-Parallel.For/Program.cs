74eaeae2a1e14c0ff35e0031c70932467332c771
Alexandre Lowawa Gaspar <71171768+Alexandre-AGKing@users.noreply.github.com>
2023-08-27 14:42:11 +0100

    Usei o método For, que serve para executar um conjunto de instruções não sequenciais
    O método Parallel.For, executa um laço for no qual as iterações podem ser executadas em paralelo.
    Esse método é equivalente à execução do laço for, só que em paralelo.

---
+++ TaskParallelLibrary-Parallel.For/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TaskParallelLibrary_Parallel.For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pressione ENTER para começar");
+            Console.ReadLine();
+            ProcessarLaco();
+            Console.WriteLine("\n\n");
+            //Chamo o método que vamos usar, isto é, For
+
+            Parallel.For(0,11,i=>Console.WriteLine(i + "\t"));
+            Console.ReadLine();
+
+        }
+        private static void ProcessarLaco() 
+        {
+        for (int i = 0; i < 11; i++) 
+            {
+            Console.WriteLine(i+" Thread : = "+ Thread.CurrentThread.ManagedThreadId +"\t");
+            Thread.Sleep(500);
+            }
+        }
+    }
+}
